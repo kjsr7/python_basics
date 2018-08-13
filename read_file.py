0455884cce3bb6d2c44fdb278a1add2d9ef41d54
kjsr7 <kommurujaishankarreddy@gmail.com>
2018-08-13 18:11:57 +0530

    try excecptions

---
+++ read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+f = open("1.txt","r")
+#check whether the file is readable
+print(f.readable())
+#printing all the contents of the file
+#print(f.read())
+#printing line
+print(f.readline())
+# r for read, w for write, a for append and r+ to write
